d119481bf5b48bed8bb814bd1bd32c1f8195844e
Neo Lichaba <neolicham@gmail.com>
2022-09-06 12:09:05 +0200

    Taco Cloud Exercise, Creating new Spring Boot Project completed

---
+++ Mobile_App/mobile-app/src/main/java/com/mobile/app/ws/MobileAppApplication.java
@@ -0,0 +1,13 @@
+package com.mobile.app.ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobileAppApplication.class, args);
+	}
+
+}
---
+++ Mobile_App/mobile-app/src/test/java/com/mobile/app/ws/MobileAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mobile.app.ws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
