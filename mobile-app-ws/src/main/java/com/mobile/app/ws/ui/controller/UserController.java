e96ffbe13103cd415ecb964c2d237de39830e6f0
Neo Lichaba <neolicham@gmail.com>
2022-09-13 23:34:47 +0200

    Section 8.23 - Storing Users Temporarily uploaded

--- mobile-app-ws/src/main/java/com/mobile/app/ws/ui/controller/UserController.java
+++ mobile-app-ws/src/main/java/com/mobile/app/ws/ui/controller/UserController.java
@@ -16,25 +16,31 @@ import org.springframework.web.bind.annotation.RestController;
 import com.mobile.app.ws.ui.model.request.UserDetailsRequestModel;
 import com.mobile.app.ws.ui.model.response.UserRest;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
 import javax.validation.Valid;
 
 
 
-@RestController  //receive HTTP requests
-@RequestMapping("users")  //http://localhost:8080/users
-						  //Will be responsible for all apparitions relating to user
-						  //Entire class is mapped to users
+@RestController // receive HTTP requests
+@RequestMapping("users") // http://localhost:8080/users
+							// Will be responsible for all apparitions relating to user
+							// Entire class is mapped to users
 public class UserController {
 	
+	Map<String,UserRest>users; //Map data type =key, object =UserRest
+
 	@GetMapping
-	public String getUsers(@RequestParam(value="page", defaultValue ="1") int page, 
-			@RequestParam(value="limit", defaultValue ="50") int limit,  
-	@RequestParam(value="sort", defaultValue= "desc", required = false) String sort){
+	public String getUsers(@RequestParam(value = "page", defaultValue = "1") int page,
+			@RequestParam(value = "limit", defaultValue = "50") int limit,
+			@RequestParam(value = "sort", defaultValue = "desc", required = false) String sort) {
 		return "get user was called with page = " + page + " and limit = " + limit + " and sort = " + sort;
 	}
-	
-	//Bind method to HTTP request
-	//Receive value in either JSON/XML value
+
+	// Bind method to HTTP request
+	// Receive value in either JSON/XML value
 	@GetMapping(path = "/{userId}", 
 			produces= {
 					MediaType.APPLICATION_XML_VALUE, 
@@ -44,46 +50,45 @@ public class UserController {
 	//Obtain user info
 	public ResponseEntity <UserRest> getUser (@PathVariable String userId) {
 		
-		//Create instance of UserRest
-		UserRest returnValue = new UserRest();
-		returnValue.setEmail("theOne@test.com");
-		returnValue.setFirstName("Neo");
-		returnValue.setLastName("Lichi");
-		
-		
-		return new ResponseEntity<UserRest>(returnValue, HttpStatus.OK);
-	}
+		if(users.containsKey(userId)) {
+			return new ResponseEntity<>(users.get(userId), HttpStatus.OK);}
+			else {
+				return new ResponseEntity<>(users.get(userId), HttpStatus.NO_CONTENT);
+			}
+		}	
 	
 	
-	
-	//Responds to POST request
-	@PostMapping(
-			consumes= {
-					MediaType.APPLICATION_XML_VALUE, 
-					MediaType.APPLICATION_JSON_VALUE
-					},
-			produces= {
-					MediaType.APPLICATION_XML_VALUE, 
-					MediaType.APPLICATION_JSON_VALUE
-					}) 
+
+	// Responds to POST request
+	@PostMapping(consumes = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE }, produces = {
+			MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE })
 	public ResponseEntity<UserRest> createUser(@Valid @RequestBody UserDetailsRequestModel userDetails) {
-		
+
 		UserRest returnValue = new UserRest();
 		returnValue.setEmail(userDetails.getEmail());
 		returnValue.setFirstName(userDetails.getFirstName());
 		returnValue.setLastName(userDetails.getLastName());
-		
+
+		String userId = UUID.randomUUID().toString();
+		returnValue.setUserId(userId);
+
+		// check if there are any users
+		if (users == null)
+			users = new HashMap<>();
+		users.put(userId, returnValue); //key, value
+
 		return new ResponseEntity<UserRest>(returnValue, HttpStatus.OK);
 
 	}
-	
+
 	@PutMapping
 	public String updateUser() {
 		return "update user was called";
 	}
-	
+
 	@DeleteMapping
 	public String deleteUser() {
 		return "delete user was called";
 	}
 }
+
