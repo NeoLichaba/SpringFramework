307d4bbb32c1171d1e19015b19765e2ea5ac8cc1
Neo Lichaba <neolicham@gmail.com>
2022-10-10 10:59:31 +0200

    Initial commit

---
+++ springboot-buildingblocks/src/main/java/com/stacksimplifyrestservices/SpringbootBuildingblocksApplication.java
@@ -0,0 +1,13 @@
+package com.stacksimplifyrestservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBuildingblocksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBuildingblocksApplication.class, args);
+	}
+
+}
