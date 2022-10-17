aaa3deeac63e3c436afb0f4418e0ee1a532fabe7
Neo Lichaba <98019180+NeoLichaba@users.noreply.github.com>
2022-10-17 22:49:07 +0200

    Experiencing white label error

--- coronavirus-tracker/coronavirus-tracker/src/main/java/com/example/coronavirustracker/controllers/HomeController.java
+++ coronavirus-tracker/coronavirus-tracker/src/main/java/com/example/coronavirustracker/controllers/HomeController.java
@@ -1,14 +1,16 @@
 package com.example.coronavirustracker.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller	
+@Controller	//renders UI 
 public class HomeController {
 	
 	@GetMapping("/") //root template	
 	public String home() {
+		//model.addAttribute("testName", "TEST");
 		return "home"; //returns what's in the home.html file
 	}
-
+ 
 }
