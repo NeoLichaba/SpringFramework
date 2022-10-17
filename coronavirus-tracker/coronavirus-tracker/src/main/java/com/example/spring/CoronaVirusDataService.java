8eae986b637633afe722d35c1e5c61cf89c506e8
Neo Lichaba <98019180+NeoLichaba@users.noreply.github.com>
2022-10-17 18:07:58 +0200

    20:56, String Reader, Parsing csv

--- coronavirus-tracker/coronavirus-tracker/src/main/java/com/example/spring/CoronaVirusDataService.java
+++ coronavirus-tracker/coronavirus-tracker/src/main/java/com/example/spring/CoronaVirusDataService.java
@@ -1,6 +1,9 @@
 package com.example.spring;
 
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -8,6 +11,8 @@ import java.net.http.HttpResponse;
 
 import javax.annotation.PostConstruct;
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
 import org.springframework.stereotype.Service;
 
 //Class reads data from Corona Virus file
@@ -32,7 +37,18 @@ public class CoronaVirusDataService {
 		//obtain response by sending client request
 		//body taken and returned as a string
 		HttpResponse<String> httpResponse = client.send(request, HttpResponse.BodyHandlers.ofString());
-		System.out.println(httpResponse.body());
+		
+		//Creating StringReader
+		StringReader csvBodyReader = new StringReader(httpResponse.body());
+		
+		//Header Auto Detection
+
+		Iterable<CSVRecord> records = CSVFormat.DEFAULT.parse(csvBodyReader);
+		for (CSVRecord record : records) {
+		    String state = record.get("Province/State");
+		    System.out.println(state);
+		    
+		}
 	}
 
 }
