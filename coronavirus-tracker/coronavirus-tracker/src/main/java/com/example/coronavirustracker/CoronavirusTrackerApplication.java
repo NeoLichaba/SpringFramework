fd6df80fccd65e5b0f6beb240570cb4643ea3b86
Neo Lichaba <98019180+NeoLichaba@users.noreply.github.com>
2022-10-19 11:18:22 +0200

    Completed CoronaVirus App

--- coronavirus-tracker/coronavirus-tracker/src/main/java/com/example/coronavirustracker/CoronavirusTrackerApplication.java
+++ coronavirus-tracker/coronavirus-tracker/src/main/java/com/example/coronavirustracker/CoronavirusTrackerApplication.java
@@ -1,7 +1,9 @@
+package com.example.coronavirustracker;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.context.annotation.ComponentScan;
+
 
 @SpringBootApplication
 
